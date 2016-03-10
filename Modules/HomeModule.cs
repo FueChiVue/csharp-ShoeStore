69de71f1587c3794d8fe687df814a3757cc24730
Jay <jaywhang83@gmail.com>
2016-03-10 12:02:49 -0800

    Add page for adding stores to specific brand and adding brands to specific store

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -45,7 +45,45 @@ namespace ShoeStore
       {
         Store newStore = new Store(Request.Form["store-name"]);
         newStore.Save();
-        return View["success.cshtml"]; 
+        return View["success.cshtml"];
+      };
+
+      Get["/brand/{id}"] = parameters =>
+      {
+        Dictionary<string, object> model = new Dictionary<string, object> ();
+        Brand selectedBrand = Brand.Find(parameters.id);
+        List<Store> brandStores = selectedBrand.GetStores();
+        List<Store> allStores = Store.GetAll();
+        model.Add("brand", selectedBrand);
+        model.Add("brandStores", brandStores);
+        model.Add("allStores", allStores);
+        return View["brand.cshtml", model];
+      };
+      Post["/brand/add_store"] = _ =>
+      {
+        Brand brand = Brand.Find(Request.Form["brand-id"]);
+        Store store = Store.Find(Request.Form["store-id"]);
+        brand.AddStore(store);
+        return View["success.cshtml"];
+      };
+
+      Get["/store/{id}"] = parameters =>
+      {
+        Dictionary<string, object> model = new Dictionary<string, object> ();
+        Store selectedStore = Store.Find(parameters.id);
+        List<Brand> storeBrands = selectedStore.GetBrands();
+        List<Brand> allBrands = Brand.GetAll();
+        model.Add("store", selectedStore);
+        model.Add("storeBrands", storeBrands);
+        model.Add("allBrands", allBrands);
+        return View["store.cshtml", model];
+      };
+      Post["/store/add_brand"] = _ =>
+      {
+        Brand brand = Brand.Find(Request.Form["brand-id"]);
+        Store store = Store.Find(Request.Form["store-id"]);
+        store.AddBrand(brand);
+        return View["success.cshtml"];
       };
     }
   }
